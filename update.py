5d1c47df79434e494c57848190537728774bee55
celebrater46 <tkgm45nbchk@gmail.com>
2024-06-23 22:23:21 +0900

    added log

--- update.py
+++ update.py
@@ -9,23 +9,18 @@ update_info = input('What did you update?: ')
 # update_ver = "1.1"
 # update_info = "テストしたぞ。"
 
-# f = open("readme.txt", "r")
 f = open("readme.txt", 'r', encoding='utf-8')
-# with open("readme.txt", 'r', encoding='utf-8') as f:
 lines = f.readlines()
 f.close()
-# print(lines)
+
 f2 = open("readme.txt", 'w', encoding='utf-8')
-# with open("readme.txt", 'w', encoding='utf-8') as f2:
 for line in lines:
-    print(line.find("バージョン情報"))
     if line.find("バージョン情報") > -1:
         f2.write(line)
         now = datetime.now()
         datestr = now.strftime("%Y-%m-%d %H:%M:%S")
         f2.write(f"{datestr} - v{update_ver}: {update_info}\n")
+        logging.info(f"Write: {datestr} - v{update_ver}: {update_info}")
     else:
         f2.write(line)
 f2.close()
-
-# f.close()
